f5483699534b1977ba88784a81c9b18c339d6de4
cagdasald <cagdas.aldogan00@gmail.com>
2023-07-22 15:22:43 +0300

    01

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+  import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+  import { useState } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  export default function App() {
+    const [goal, setGoal] = useState('');
+    const [goals, setGoals] = useState<string[]>([]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    function goalInputHandler(entredText: string) {
+      setGoal(entredText);
+    }
+
+    function addGoalHandler() {
+      setGoals(currentCourseGoals => [...currentCourseGoals, goal]);
+    }
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputWrapper}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Type your goal here"
+            onChangeText={goalInputHandler}
+          />
+          <Button onPress={addGoalHandler} title="Add Goal" />
+        </View>
+        <View style={styles.goalsWrapper}>
+            {
+              goals.map((goal) => <Text key={goal}>{goal}</Text>)
+            }
+        </View>
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 50,
+      paddingHorizontal: 16,
+    },
+    inputWrapper: {
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginBottom: 24,
+      borderBottomWidth: 1,
+      borderBottomColor: "#000",
+    },
+    textInput: {
+      borderWidth: 1,
+      borderColor: "#cccccc",
+      width: "70%",
+      marginRight: 8,
+      padding: 8,
+    },
+    goalsWrapper: {
+      flex: 5,
+      flexDirection: "column"
+    },
+  });
